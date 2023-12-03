85ad60ae406bd939ec0ed215ad96f9447b510d0b
Alan Cristian Martinez Genaro <AlanCristianMG>
2023-12-03 17:33:22 -0600

    pantalla inicio

--- src/components/startPage.jsx
+++ src/components/startPage.jsx
@@ -1,21 +1,30 @@
-
-import React from 'react';
-import {Image} from "@nextui-org/react";
-import  '../css/startPage.css';
-import LogoWhite from '../img/logo/Logo-White-sf.png';
-
+import React, { useState } from 'react';
+import '../css/startPage.css';
+import Logo from '../img/logo/Logo-White-sf.png';
 
 function StartPage() {
-    return(
-        <div class="container">
-            <div class="background"></div>
-            <div class="content">
-                <img className="logo" src={LogoWhite} alt="" />
-                <h1 className='nameCompany'>AMI</h1>
-            </div>
-        </div>
+  const [isRotating, setIsRotating] = useState(false);
 
-    )
+  return (
+    <div className="app">
+      <div className="blur-bg"></div>
+      <div className="content">
+        <img
+          src={Logo}
+          alt="Logo"
+          className={`logo ${isRotating ? 'rotate' : ''}`}
+          onClick={() => {(isRotating)?setIsRotating(false):setIsRotating(true)}}
+          
+        />
+        <h1 className='logoName'>AMI</h1>
+        <p className='brandText'></p>
+        <div className="buttons">
+          <button className="button">Botón 1</button>
+          <button className="button">Botón 2</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StartPage;
