e3c9c15315bffa7ceb8186af5fce0a29c8a4bfb2
Alan Cristian Martinez Genaro <AlanCristianMG>
2023-12-03 18:47:59 -0600

    avance StartPage

--- src/components/startPage.jsx
+++ src/components/startPage.jsx
@@ -17,10 +17,19 @@ function StartPage() {
           
         />
         <h1 className='logoName'>AMI</h1>
-        <p className='brandText'></p>
+        <p className='brandText'>
+            Tu compañero en el camino hacia una vida más saludable. 
+            Con la mezcla de tecnología avanzada y un enfoque pensado en ti
+            <strong> ¡Es hora de cuidar de ti de una manera fácil y divertida! ¡Unete!</strong></p>
         <div className="buttons">
-          <button className="button">Botón 1</button>
-          <button className="button">Botón 2</button>
+          <button class="button">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+          </svg>
+          <div class="text">
+            Unirse
+          </div>
+          </button>
         </div>
       </div>
     </div>
